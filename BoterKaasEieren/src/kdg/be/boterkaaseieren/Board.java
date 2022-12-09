1ae039ef671e9657ee97ab3049eb65e357877900
aaronvanhove <vanhoveaaron@gmail.com>
2022-12-09 09:01:47 +0100

    initial commit

--- BoterKaasEieren/src/kdg/be/boterkaaseieren/Board.java
+++ BoterKaasEieren/src/kdg/be/boterkaaseieren/Board.java
@@ -1,8 +1,18 @@
 package kdg.be.boterkaaseieren;
 
+
+
 public class Board {
     private static final int WIDTH = 3;
     private static final int HEIGHT = 3;
+    private Piece[][] piece;
+    private int pieceCounter;
 
+    public Board(int piececounter) {
+        this.piece = new Piece[WIDTH][HEIGHT];
+    }
+    public void addPiece(Piece piece, int columnNumber, int rowNumber) {
+
+    }
 
 }
