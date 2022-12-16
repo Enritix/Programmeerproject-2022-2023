eb3df4aa1bd6d9b44c0a456b6f40b4d87d28006c
Enritix <crazykillergames2@hotmail.com>
2022-12-16 15:08:57 +0100

    initial commit

--- BoterKaasEieren/src/kdg/be/enrico/aaron/boterkaaseieren/BKEApplication.java
+++ BoterKaasEieren/src/kdg/be/enrico/aaron/boterkaaseieren/BKEApplication.java
@@ -1,13 +1,35 @@
 package kdg.be.enrico.aaron.boterkaaseieren;
 
+import java.util.Scanner;
+
 public class BKEApplication {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         BoterKaasEieren game = new BoterKaasEieren();
-        game.TwoPlayers();
-        game.playGame();
+        Board board = new Board();
         //if button 1v1 is pressed --> game.TwoPlayers() & game.playGame
         //if button 1vComputer is pressed --> game.OnePlayerOneComputer() & game.playGame
-        GameRules rules = new GameRules();
+        int keuze;
+        do {
+            System.out.println("What do you want to do?\n- 1: Play 1v1\n- 2: Play against computer\n- 3: Leaderboard\n- 4: Game rules\n- 5: Exit game");
+            System.out.print("Choice: ");
+            keuze = sc.nextInt();
+            if (keuze == 1) {
+                game.TwoPlayers();
+                game.playGame();
+            } else if (keuze == 2) {
+                game.OnePlayerOneComputer();
+                game.playGame();
+            } else if (keuze == 3) {
+                Score score = new Score();
+                score.getScore();
+            } else if (keuze == 4) {
+                GameRules rules = new GameRules();
+                rules.printGameRules();
+            } else if (keuze < 1 || keuze > 5){
+                System.out.println("This is not a valid choice!");
+            }
+        } while (keuze != 5);
         
 
     }
--- BoterKaasEieren/src/kdg/be/enrico/aaron/boterkaaseieren/Board.java
+++ BoterKaasEieren/src/kdg/be/enrico/aaron/boterkaaseieren/Board.java
@@ -26,19 +26,13 @@ public class Board {
         return pieceCounter == BORDER * BORDER;
     }
 
-    //TODO: hasOXO method
-
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 3; i++) {
-            sb.append(" ").append(i);
-        }
-
-        for (int i = 0; i < BORDER; i++) {
+        /*StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < BORDER; i++) {
             sb.append(" ");
             System.out.print("\n-------\n");
-            for (int j = 1; j < BORDER; j++) {
+            for (int j = 2; j < BORDER; j++) {
                 if (pieces[i][j] == null) {
                     sb.append(" ").append("|");
 
@@ -54,8 +48,44 @@ public class Board {
                 }
             }
         }
+        return sb.toString();*/
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            sb.append(String.format(" %d ", i));
+        }
+        sb.append("\n");
+        for (int i = 0; i < pieces.length; i++) {
+            for (int j = 0; j < pieces[i].length; j++) {
+                if (pieces[i][j] == null) {
+                    sb.append(" ").append(" |");
+
+                } else {
+                    if (pieces[i][j].getColor() == RED) {
+                        sb.append("O").append("|");
+                    } else {
+                        sb.append("X").append("|");
+                    }
+                }
+            }
+            if (i < pieces.length - 1) {
+                sb.append("\n---+---+---\n");
+            }
+        }
         return sb.toString();
     }
+        /*
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < pieces.length; i++) {
+            sb.append(" ").append(pieces[i][0]).append(" |");
+            sb.append(" ").append(pieces[i][1]).append(" |");
+            sb.append(" ").append(pieces[i][2]).append(" \n");
+            if (i < pieces.length - 1) {
+                sb.append("\n---+---+---\n");
+            }
+        }
+        return sb.toString();
+    }*/
+
 
     public Piece.Color hasOXO() {
         //TODO
